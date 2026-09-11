5350e67e918d5654e3f4a9355d4069fffd6ba470
Shawon <shawon@example.com>
2026-09-11 23:02:35 +0600

    Auto update playlist

--- push.bat
+++ push.bat
@@ -4,16 +4,16 @@ echo Running Python script to generate playlist...
 python generate.py
 
 echo.
-echo Pulling latest changes from GitHub...
-git pull origin main --rebase
-
-echo.
 echo Adding changes to git...
 git add .
 
 echo.
 echo Committing changes...
 git commit -m "Auto update playlist"
+
+echo.
+echo Pulling latest changes from GitHub...
+git pull origin main --rebase
 
 echo.
 echo Pushing changes to GitHub...
